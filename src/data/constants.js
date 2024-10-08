b4a5f21ae17e36a5636243a27fe339e82648bfb1
LiandarJoshua <joshuxvro@gmail.com>
2024-10-08 21:54:19 +0400

    Initial commit of my React app

---
+++ src/data/constants.js
@@ -0,0 +1,220 @@
+export const Bio = {
+  name: "Liandar Joshua",
+  roles: [
+    "Machine Learning",
+    "AI",
+    "Programmer",
+    "Student",
+  ],
+  description:
+  "I am an aspiring programmer, interested in machine learning,who's eager to gain hands on experience and work on real-world projects",
+  github: "https://github.com/LiandarJoshua",
+  resume:
+    "https://drive.google.com/file/d/1sQ3OYVzmsArMsTuUZiZ4NFLH7elznM-m/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/liandar-joshua-5b4368289/",
+};
+
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Flask",
+        image: "https://img.icons8.com/nolan/2x/python.png",
+      },
+      {
+        name:"Java",
+        image:"https://static.vecteezy.com/system/resources/previews/022/100/214/original/java-logo-transparent-free-png.png"
+      },
+      {
+        name:"C++",
+        image:"https://i1.wp.com/filipjaniszewski.com/wp-content/uploads/2016/12/C-PNG-Clipart.png?fit=550%2C380"
+      },
+      {
+        name:"Streamlit",
+        image:"https://seeklogo.com/images/S/streamlit-logo-1A3B208AE4-seeklogo.com.png"
+      }
+    ],
+  },
+ 
+  {
+    title: "Others",
+    skills: [
+      
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.gartner.com/pi/vendorimages/postman_full-life-cycle-api-management_1633960356020.png",
+      },
+
+      {
+        name: "EDA",
+        image:"https://cdn0.iconfinder.com/data/icons/big-data-3-1/128/Exploratory-Analysis-patterns-methods-discovering-data-1024.png"
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://e0.pxfuel.com/wallpapers/596/856/desktop-wallpaper-vit-logo-vellore-institute-of-technology-vector-indian-institute-logo.jpg",
+    school: "Vellore Institute Of Technology",
+    date: "Sep 2022 - May 2026",
+    grade: "9.3 CGPA",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering with Specialization in Data Science",
+  },
+  {
+    id: 1,
+    img: "https://yt3.ggpht.com/ytc/AKedOLQCWEdM-9riJXKZzHsVoXZqF4HvSVmUegKm3umo=s900-c-k-c0x00ffffff-no-rj",
+    school: "Mayoor Private School,Abu Dhabi",
+    date: "Apr 2018 - Apr 2022",
+    grade: "94%",
+    degree: "CBSE 10th and 12th",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Stock App",
+    date: "Jun 2023 - Jul 2023",
+    description:
+    "Stock Application that takes a stock ticker as input and predicts tomorrow's closing price, it also graphically represents prices of past 30 days",
+    image:
+      "https://private-user-images.githubusercontent.com/160778757/311453745-0104558d-9351-41e2-a858-6879e0c04dad.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjgyNzkwMTAsIm5iZiI6MTcyODI3ODcxMCwicGF0aCI6Ii8xNjA3Nzg3NTcvMzExNDUzNzQ1LTAxMDQ1NThkLTkzNTEtNDFlMi1hODU4LTY4NzllMGMwNGRhZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMDA3JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTAwN1QwNTI1MTBaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00MzdmMjRjZDg2MDU0MjVkN2MwZjdhYTc0NmFlNzI5OTlkOGI2ZjhiZmE3MDk2MjlmZjQxNzliNGI2MWU5YjcxJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.xafxt6iJNTZ8mey7h2P59CsW-BeUNCQOl-vT7AkRQeY",
+    tags: [
+      "Docker",
+      "AWS",
+      "Python",
+      "Deep learning",
+      "Postman",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    github: "https://github.com/LiandarJoshua/Stock-App" ,
+  },
+  {
+    id: 0,
+    title: "NyayMitra",
+    date: "September 2024",
+    description:
+      "Developed a full-stack web application AI Assistant for judiciary information.Worked on the chatbot.",
+    image:
+      "https://private-user-images.githubusercontent.com/160778757/374040710-08021602-9ee9-4f3f-8844-364ebcecbfec.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjgyNzg5MTksIm5iZiI6MTcyODI3ODYxOSwicGF0aCI6Ii8xNjA3Nzg3NTcvMzc0MDQwNzEwLTA4MDIxNjAyLTllZTktNGYzZi04ODQ0LTM2NGViY2VjYmZlYy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMDA3JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTAwN1QwNTIzMzlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT05YjBmNjdjZmE3NzdmYzVhYzlmZjkwNmY0MTU1OTY5ZGExYjZjNWI2YjUxMzIwMzkxOTIxMjU4Zjg0MDk1M2Y3JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.glRMRg0r0kG1a-QQdHhf2TkrWc527znTCBzpYvXiuFY",
+    tags: ["React Js", "Python", "GROQ", "Langchain", "RAG"],
+    category: "web app",
+    github: "https://github.com/shriharit04/NyayMitra-SIH",
+    member: [
+      {
+        name: "KV Venkatakrishnan",
+        img: "https://media.licdn.com/dms/image/v2/D5635AQEI1Mv7mbrGmg/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1722926574933?e=1728885600&v=beta&t=BGIcF2EPGayhesJbpMooeZyEN3G2pJvgApHwFnHXmes",
+        linkedin: "https://www.linkedin.com/in/venkatakrishnan-kv-186339256"
+      },
+      {
+        name: "Shrihari",
+        img: "https://media.licdn.com/dms/image/v2/D5603AQHEJl3W3AdZyg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724781296702?e=1733961600&v=beta&t=3FR4GPQjW8TjHCBaWxiBu2a51Rs_zoSUeDzgRSZ9jSY",
+        linkedin: "https://www.linkedin.com/in/ntshrihari/",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "ASL Sign Language Detection",
+    date: "June 2024",
+    description:
+    "Designed a Sign Language Detection app that takes live input from the webcam, interprets ASL sign language and reads out the sentence.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:337/0*6aEBXcTf3YxIOund.png",
+    tags: [
+      "Computer Vision",
+      "Python",
+      "Flask"
+    ],
+    category: "web app",
+    github: "https://github.com/LiandarJoshua/ASL-Detection",
+  },
+  {
+    id: 2,
+    title: "News App(Mini Project)",
+    date: "Feb 2023 - Mar 2023",
+    description:
+    "Developed a News Web App, capable of dynamically changing news daily and taking user input to search for relevant news, and displaying the articles.",
+    image:
+      "https://private-user-images.githubusercontent.com/160778757/374038543-8cc46b02-8a3d-4ed6-8bc0-4b0938360aa9.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjgyNzgyNTgsIm5iZiI6MTcyODI3Nzk1OCwicGF0aCI6Ii8xNjA3Nzg3NTcvMzc0MDM4NTQzLThjYzQ2YjAyLThhM2QtNGVkNi04YmMwLTRiMDkzODM2MGFhOS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMDA3JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTAwN1QwNTEyMzhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00OWFkNDNjMmY2MDdkNWE1NmJmMDQyN2YyMDZlZjFmZmVlYzMxNjc1YWE0M2I1NmJkMzQ3M2Y5MDJkYTNhM2ZiJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.4Cm03zh73eOduxpYz7QwI4LsIMFhl67F5AqWPYrnsUA",
+    tags: ["HTML", "CSS", "JavaScript", "React"],
+    category: "web app",
+    github: "https://github.com/LiandarJoshua/NewsAPP-Web-Programming-Course-Mini-Project-",
+  },
+  {
+    id: 3,
+    title: "Traffic Management Dashboard",
+    date: "Jul 2023",
+    description:
+    "A Basic Dashboard for Traffic management, that is capable of taking a video input and detecting vehicles on each lane,indicating traffic density. Integrated with SQLite.",
+    image:
+      "https://private-user-images.githubusercontent.com/160778757/373865469-c2535b3a-9daa-4bf1-9f80-f9c17b6ac608.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjgyNzgzNzMsIm5iZiI6MTcyODI3ODA3MywicGF0aCI6Ii8xNjA3Nzg3NTcvMzczODY1NDY5LWMyNTM1YjNhLTlkYWEtNGJmMS05ZjgwLWY5YzE3YjZhYzYwOC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMDA3JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTAwN1QwNTE0MzNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xOTdhYjU5M2QwNTViNzg3Y2U3Y2UxZTg2Y2IzNjhmOGVjYTAwYTExYmUyMzA3ZWQxNDdjMTMyODBlZTMwNDdiJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.lSoKE-teXVoZ7AHByJHx62WuCnHGpgPja-k5VFDWoZ4",
+    tags: ["SQLite","Computer Vision","YOLO","Streamlit"],
+    category: "web app",
+    github: "https://github.com/LiandarJoshua/Traffic-Management-Dashboard",
+    
+  },
+];
+
+
