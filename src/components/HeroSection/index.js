711c24db90789abbc68ccae0c14b35ab3c4866b7
JoshuxVro <joshualiandar22@gmail.com>
2024-10-23 16:18:37 +0400

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -15,18 +15,7 @@ const HeroSection = () => {
                 <HeroInnerContainer >
                     <HeroLeftContainer id="Left">
                         <Title>Hi, I am <br /> {Bio.name}</Title>
-                        <TextLoop>
-                            
-                            <Span>
-                                <Typewriter
-                                    options={{
-                                        strings: Bio.roles,
-                                        autoStart: true,
-                                        loop: true,
-                                    }}
-                                />
-                            </Span>
-                        </TextLoop>
+                       
                         <SubTitle>{Bio.description}</SubTitle>
                         <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
                     </HeroLeftContainer>
