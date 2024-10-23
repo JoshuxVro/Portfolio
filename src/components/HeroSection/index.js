23f95c49e85f8c1435b1081fb121cc5f6e441544
JoshuxVro <joshualiandar22@gmail.com>
2024-10-23 17:40:34 +0400

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -19,7 +19,7 @@ const HeroSection = () => {
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
-                        <Img src="https://ibb.co/2g0cQLL" alt="hero-image" />
+                        <Img src="https://i.ibb.co/t8wYtNN/image1.jpg" alt="hero-image" />
                     </HeroRightContainer>
                 </HeroInnerContainer>
             </HeroContainer>
